5cfd31245453bdb94df64da7f63cf6c239ef6de4
GaneshK <ganeshkrishna2000@gmail.com>
2022-10-19 14:54:55 -0400

    Firebase works now

--- app/src/main/java/com/example/mte460project/MainActivity.java
+++ app/src/main/java/com/example/mte460project/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.mte460project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+        Context n = this;
+        myRef.child("newPath").setValue("HELLO").addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(n,  e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Toast.makeText(n, "SUCESS", Toast.LENGTH_LONG);
+            }
+        });
     }
 }
